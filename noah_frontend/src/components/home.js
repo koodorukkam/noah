cdc2f28674a891ca6ccadaf63c023aa13c5b51ce
Nakul Ezhuthupally Sibiraj <nakule@gmail.com>
2018-08-27 21:37:37 +0530

    cleanup the home component

--- noah_frontend/src/components/home.js
+++ noah_frontend/src/components/home.js
@@ -5,46 +5,7 @@ import { connect } from 'react-redux';
 class HomeView extends React.Component {
     render() {
         return (
-<div>
-    <nav className="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
-      <div className="container">
-        <a className="navbar-brand js-scroll-trigger" href="#page-top">Koodorukkam</a>
-        <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-          Menu
-          <i className="fas fa-bars"></i>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarResponsive">
-          <ul className="navbar-nav text-uppercase ml-auto">
-            <li className="nav-item">
-              <a className="nav-link js-scroll-trigger" href="#services">Services</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link js-scroll-trigger" href="#portfolio">Portfolio</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link js-scroll-trigger" href="#about">About</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link js-scroll-trigger" href="#team">Team</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link js-scroll-trigger" href="#contact">Contact</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-    <header className="masthead">
-      <div className="container">
-        <div className="intro-text">
-          <div className="intro-lead-in">Use the unused utensils and appliances at your home to make a BIG difference</div>
-          <div className="intro-heading text-uppercase">Koodorukkam</div>
-          <a className="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="#services">Tell Me More</a>
-        </div>
-      </div>
-    </header>
-
-</div>
+          <h1>Koodorukkam</h1>
         )
     }
 }
