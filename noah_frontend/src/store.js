352f33b8570033cdf34a0b1931a3c2f7dc5ec324
Nakul Ezhuthupally Sibiraj <nakule@gmail.com>
2018-08-28 08:12:35 +0000

    Add redux logger to the frontend application

--- noah_frontend/src/store.js
+++ noah_frontend/src/store.js
@@ -1,6 +1,7 @@
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import reduxThunk from 'redux-thunk';
+import logger from 'redux-logger'
 
 import reducers from './reducers'
 
@@ -10,6 +11,7 @@ export default function configureStore(initialState) {
         initialState,
         composeWithDevTools(
             applyMiddleware(
+                logger,
                 reduxThunk
             )
         ))
