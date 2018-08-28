cd35a968f4df6187ffd43c25e25dd0c4ccc489e1
Nakul Ezhuthupally Sibiraj <nakule@gmail.com>
2018-08-28 10:40:45 +0530

    serve static files also through django

--- noah/urls.py
+++ noah/urls.py
@@ -13,11 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import os
+
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('api/v1/', include('noah_core.urls'))
-]
+    path('api/v1/', include('noah_core.urls')),
+] + static("/", document_root=os.path.join(settings.BASE_DIR, "noah_frontend", "dist"), path="index.html")
