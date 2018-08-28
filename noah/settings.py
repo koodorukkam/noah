17dc7d0e07bb3b587d10846b5c4daba44553b742
Nakul Ezhuthupally Sibiraj <nakule@gmail.com>
2018-08-28 09:35:51 +0530

    Enable cross-origin requests

--- noah/settings.py
+++ noah/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'm^s*zw*xfi_!8*q_g9x0uhpanmnoic=qgt3d!bilca2wm#843s'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -38,10 +38,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'corsheaders',
+
     'noah_core.apps.NoahCoreConfig'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -119,3 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Custom
+
+CORS_ORIGIN_ALLOW_ALL = True
