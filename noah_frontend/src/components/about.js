c8d614f84950e45124f3ced201b84deccad34a3d
Nakul Ezhuthupally Sibiraj <nakule@gmail.com>
2018-08-31 15:08:40 +0530

    Add contact-us email

--- noah_frontend/src/components/about.js
+++ noah_frontend/src/components/about.js
@@ -9,7 +9,10 @@ export default () => (
           <hr className="light my-4" />
           <p className="text-faded mb-4 text-justify">
           We are a group of volunteers who have come together for a cause -to help the people affected by the floods. We aim at helping them get back to their normal lives is a better, stronger way. 
-          We believe in the power of what people can achieve when they come together. We are joined by many like minded organisations like ……, all with one purpose - to come together and help. 
+          <br/><br/>
+          We believe in the power of what people can achieve when they come together. We are joined by many like minded organisations like G-Tech and NASSCOM, all with one purpose - to come together and help.
+          <br/><br/>
+          For any other queries, contact - <a style={{color: "white"}} href="mailto:koodorukkam@gmail.com">koodorukkam@gmail.com</a>
           </p>
           <a className="btn btn-light btn-xl sr-button" href="#donate">Donate Now!</a>
         </div>
