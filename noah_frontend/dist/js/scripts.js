ca23a2d13ee4e5595078214afe973554fb1fb7ee
shamin.meerankutty <shamin.meerankutty@fullcontact.com>
2018-08-31 19:19:02 +0530

    Added malayalam and english text slider

---
+++ noah_frontend/dist/js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+    items: 1,
+    margin:100,
+    autoplay: true,
+    autoplayTimeout:10000,
+    autoplaySpeed: 1000,
+    autoplayHoverPause:true,
+    loop: true
+  });
+});
--- noah_frontend/src/components/header.js
+++ noah_frontend/src/components/header.js
@@ -2,21 +2,24 @@ import React from 'react'
 
 export default () => (
     <header className="masthead text-center text-white d-flex">
-    <div className="container my-auto">
-        <div className="row">
-            <div className="col-lg-10 mx-auto">
-                <h1 className="text-uppercase">
-                <strong>കൂടൊരുക്കാം</strong>
-                </h1>
-                <hr />
-            </div>
-            <div className="col-lg-8 mx-auto">
-                <h3>Use the unused utensils and appliances at your home to make a BIG difference</h3>
-                <hr/>
-                <p className="text-justify text-faded mb-5">Those small appliances and utensils that are lying around at our homes, unused or rarely used, could make a huge difference to the people who have lost everything. An unused iron box, a dinner set you got as a gift, an emergency lamp you rarely use and much more. Donate them to the people who have lost everything in the flood and be part of rebuilding Kerala. </p>
-                <a className="btn btn-primary btn-xl js-scroll-trigger" href="#donate">Donate Appliances or Utensils</a>
+        <div className="container my-auto">
+            <div className="row">
+                <div className="col-lg-10 mx-auto">
+                    <h1 className="text-uppercase">
+                        <strong>കൂടൊരുക്കാം</strong>
+                    </h1>
+                    <hr />
+                </div>
+                <div className="col-lg-8 mx-auto">
+                    <h3>Use the unused utensils and appliances at your home to make a BIG difference</h3>
+                    <hr />
+                    <div className="owl-carousel">
+                        <p className="text-justify text-faded mb-5 content">നിങ്ങളുടെ വീട്ടിൽ ഉപകാരമില്ലാതെയും ഉപയോഗിക്കപ്പെടാതെയും കിടക്കുന്ന ചെറിയ ഉപകരണങ്ങൾക്ക് എല്ലാം നഷ്ടപ്പെട്ട് തകർന്നിരിക്കുന്ന കുടുംബങ്ങളുടെ ജീവിതത്തിൽ വലിയ മാറ്റമുണ്ടാക്കാൻ സാധിക്കും. നിങ്ങൾക്ക് ഉപഹാരമായൊ ലഭിച്ച ഒരു ക്രോക്കറി സെറ്റിനും ജനറേറ്റർ ബാക്കപ്പ്‌ ഉള്ള നിങ്ങളുടെ അപ്പാർട്ട്മെന്റിൽ പൊടി പിടിച്ച്‌ കിടക്കുന്ന എമർജ്ജെൻസി ലാമ്പിനും ഒരാളുടെ ജീവിതത്തിലും വെളിച്ചം കൊണ്ട് വരാൻ കഴിയും. പ്രളയത്തിൽ എല്ലാം നഷ്ടപ്പെട്ടവർക്ക് ഇവ നൽകി നമുക്ക് നമ്മുടെ കേരളത്തെ പുനർനിർമ്മിക്കാം.</p>
+                        <p className="text-justify text-faded mb-5 content">Those small appliances and utensils that are lying around at our homes, unused or rarely used, could make a huge difference to the people who have lost everything. An unused iron box, a dinner set you got as a gift, an emergency lamp you rarely use and much more. Donate them to the people who have lost everything in the flood and be part of rebuilding Kerala. </p>
+                    </div>
+                    <a className="btn btn-primary btn-xl js-scroll-trigger" href="#donate">Donate Appliances or Utensils</a>
+                </div>
             </div>
         </div>
-    </div>
     </header>
 )
