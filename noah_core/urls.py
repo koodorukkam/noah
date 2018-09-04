d430382d67bf2de698e56120b1306c92fa6353f4
Nakul Ezhuthupally Sibiraj <nakule@gmail.com>
2018-09-04 11:49:53 +0530

    Add new donation export view, accessible only by admins

--- noah_core/urls.py
+++ noah_core/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from .views import InitView, DonationView
+from .views import InitView, DonationView, ExportView
 
 urlpatterns = [
     path('init/', InitView.as_view()),
-    path('donation.register/', DonationView.as_view())
+    path('donation.register/', DonationView.as_view()),
+
+    path('export.donations/', ExportView.as_view(), name="donations-export")
 ]
