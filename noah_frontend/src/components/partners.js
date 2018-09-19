e9b9ba44e9216df84d1723c51d90c8a445f27315
shamin.meerankutty <shamin.meerankutty@fullcontact.com>
2018-09-19 21:19:43 +0530

    Added kaleyra and rotory as partners

--- noah_frontend/src/components/partners.js
+++ noah_frontend/src/components/partners.js
@@ -31,8 +31,21 @@ export default () => (
             <h2 className="mb-4">Volunteer Partners</h2>
             <hr /><br /><br />
             <div className="row">
-                <div className="col-md-6 offset-md-3">
+                <div className="col-md-6">
                     <a target="_blank" href="http://www.prathidhwani.org/"><img className="img-fluid" src="img/partners/PrathiDhwani.png" /></a>
+                </div>
+                <div className="col-md-6">
+                    <a target="_blank" href="http://www.rotarycochin.org/"><img className="img-fluid" src="img/partners/rotary.png" /></a>
+                </div>
+            </div>
+            <div className="clearfix"></div>
+        </div>
+        <div className="container text-center">
+            <h2 className="mb-4">Communication Partner</h2>
+            <hr /><br /><br />
+            <div className="row">
+                <div className="col-md-6 offset-md-3">
+                    <a target="_blank" href="https://www.kaleyra.com/"><img className="img-fluid" src="img/partners/kaleyra.png" /></a>
                 </div>
             </div>
             <div className="clearfix"></div>
