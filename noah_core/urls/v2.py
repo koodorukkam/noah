7a0df06e890383cd12335e6cd4f6f2e6e8392327
Nakul Ezhuthupally Sibiraj <nakule@gmail.com>
2018-09-23 12:15:22 +0530

    Simple authenticated view to get info about the authenticated user

--- noah_core/urls/v2.py
+++ noah_core/urls/v2.py
@@ -5,5 +5,7 @@ from ..views import auth
 # All legacy URL patterns
 urlpatterns = [
     path('auth.requestOTP', auth.RequestOTP.as_view()),
-    path('auth.login', auth.Login.as_view())
+    path('auth.login', auth.Login.as_view()),
+
+    path('user.me', auth.CurrentUser.as_view())
 ]
